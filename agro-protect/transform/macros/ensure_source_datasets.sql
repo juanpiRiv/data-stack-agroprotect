d9b55ad202678318fbf4d782846c8cf2afd8763f
juanpiRiv <juanpirivero015@gmail.com>
2026-03-21 11:44:36 -0300

    feat: new updates for pipeliens

--- agro-protect/transform/macros/ensure_source_datasets.sql
+++ agro-protect/transform/macros/ensure_source_datasets.sql
@@ -3,7 +3,11 @@
     {%- set nasa_on = env_var('DBT_ENABLE_RAW_NASA', 'true') | lower in ['true', '1', 'yes'] -%}
     {%- set source_datasets = [tap_agro_ds] -%}
     {%- if nasa_on -%}
-        {%- set source_datasets = source_datasets + ['raw_nasa'] -%}
+        {%- set _nds = env_var('DBT_RAW_NASA_DATASET', '') | trim -%}
+        {%- set nasa_ds = _nds if _nds else (target.name ~ '_tap_agro') -%}
+        {%- if nasa_ds != tap_agro_ds -%}
+            {%- set source_datasets = source_datasets + [nasa_ds] -%}
+        {%- endif -%}
     {%- endif -%}
     {%- set project = target.project -%}
     {%- set location = target.location if target.location is defined and target.location else 'US' -%}
